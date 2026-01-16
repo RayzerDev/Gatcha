3b97b562d017dc8b4d701db3885edf25fa51ac3c
Louis KARAMUCKI <louis.karamucki@etu.imt-nord-europe.fr>
2026-01-16 16:02:52 +0100

    Add DTO info and more on swagger

--- auth/src/main/java/fr/imt/nord/fisa/ti/gatcha/auth/configuration/OpenApiConfig.java
+++ auth/src/main/java/fr/imt/nord/fisa/ti/gatcha/auth/configuration/OpenApiConfig.java
@@ -5,14 +5,34 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration OpenAPI/Swagger pour la documentation de l'API.
+ * <p>
+ * La documentation est accessible à : http://localhost:8080/swagger-ui.html
+ */
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Auth")
-                        .version("1.0")
-                        .description("Documentation de l'API d'authentification"));
+                        .title("API d'Authentification Gatcha")
+                        .version("1.0.0")
+                        .description("""
+                                API de gestion de l'authentification pour le système Gatcha.
+                                
+                                Cette API permet de :
+                                - Enregistrer de nouveaux utilisateurs
+                                - Authentifier les utilisateurs existants
+                                - Générer des tokens de session valables 1 heure
+                                - Vérifier la validité des tokens
+                                - Renouveler automatiquement les tokens valides
+                                
+                                Format du token : username-YYYY/MM/DD-HH:mm:ss (encrypté avec BCrypt)
+                                
+                                Les tokens sont automatiquement prolongés de 1 heure à chaque vérification réussie.
+                                """)
+                );
     }
 }
--- auth/src/main/java/fr/imt/nord/fisa/ti/gatcha/auth/dto/user/InputRegisterDTO.java
+++ auth/src/main/java/fr/imt/nord/fisa/ti/gatcha/auth/dto/user/InputRegisterDTO.java
@@ -1,14 +1,20 @@
 package fr.imt.nord.fisa.ti.gatcha.auth.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Données d'enregistrement utilisateur")
 public class InputRegisterDTO {
+
     @NotBlank(message = "Username cannot be blank")
+    @Schema(description = "Nom d'utilisateur souhaité", example = "john", required = true)
     private String username;
+
     @NotBlank(message = "Password cannot be blank")
+    @Schema(description = "Mot de passe", example = "password123", required = true)
     private String password;
 }
